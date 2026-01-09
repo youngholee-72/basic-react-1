1e0b9f39e9e4649d9792dd886b2a0632565b2b74
likedream <likedream@epience.com>
2026-01-09 12:33:25 +0900

    jsonplaceholder 이용하는 코드 제거함

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,10 @@
-import { useState } from "react";
 import JsonApp from "./JsonApp";
-import JsonplaceholderApp from "./JsonplaceholderApp";
 
 function App() {
-
-  const [activeTab, setActiveTab] = useState<'json-server'|'json-jsonplaceholder'>('json-server');
   
   return (
     <>
-      <div style={{ display: "flex", gap: 8, marginBottom: 16, borderBottom: '1px solid #ddd' }}>
-            <button 
-              onClick={() => setActiveTab('json-server')}
-              style={{ 
-                padding: '8px 16px', 
-                border: 'none', 
-                background: activeTab === 'json-server' ? '#0078d4' : 'transparent',
-                color: activeTab === 'json-server' ? 'white' : '#0078d4',
-                cursor: 'pointer',
-                borderBottom: activeTab === 'json-server' ? '2px solid #0078d4' : '2px solid transparent'
-              }}
-            >
-              JSON-SERVER
-            </button>
-            <button 
-              onClick={() => setActiveTab('json-jsonplaceholder')}
-              style={{ 
-                padding: '8px 16px', 
-                border: 'none', 
-                background: activeTab === 'json-jsonplaceholder' ? '#0078d4' : 'transparent',
-                color: activeTab === 'json-jsonplaceholder' ? 'white' : '#0078d4',
-                cursor: 'pointer',
-                borderBottom: activeTab === 'json-jsonplaceholder' ? '2px solid #0078d4' : '2px solid transparent'
-              }}
-            >
-              JSON-PLACEHOLDER
-            </button>
-          </div>
-
-      { activeTab == 'json-server' && <JsonApp />}
-      { activeTab == 'json-jsonplaceholder' && <JsonplaceholderApp />}
+    <JsonApp />
     </>
   );
 }
--- src/JsonApp.tsx
+++ src/JsonApp.tsx
@@ -19,9 +19,6 @@ function JsonApp() {
       const data = await fetchPosts();
       const top = data.slice(0, 20);
 
-      console.log(top);
-     // console.log(JSON.stringify(top, null, 2))
-
       setPosts(top);
     } catch (error) {
       console.error(error);
@@ -42,26 +39,11 @@ function JsonApp() {
   }
 
   useEffect(() => {
-    console.log(`selectedId : ${selectedId}`);
     if (selectedId === null) {
       setSelectedPost(null);
       return;
     }
-
     loadPost(selectedId);
-    // (async () => {
-    //   console.log(
-    //     "selectedId Changed so selectedPost changed, setSelectedPost cause reload again?"
-    //   );
-
-    //   if (selectedId === null) {
-    //     setSelectedPost(null);
-    //     return;
-    //   }
-
-    //   const data = await fetchPost(selectedId);
-    //   setSelectedPost(data);
-    // })();
   }, [selectedId]);
 
   return (
@@ -114,20 +96,6 @@ function JsonApp() {
               })}
           </ul>
         </section>
-
-        {/* <section>
-          {selectedId && selectedPost && (
-            <>
-              <h3 style={{ marginBottom: 8 }}>{selectedPost.title}</h3>
-              <p style={{ whiteSpace: "pre-wrap", marginTop: 0 }}>
-                {selectedPost.body}
-              </p>
-              <small>
-                postId: {selectedPost.id} / userId: {selectedPost.userId}
-              </small>
-            </>
-          )}
-        </section> */}
       </div>
     </>
   );
