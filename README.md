6cd3092ea193f239a0decc54d3a4f7c1c135e176
likedream <likedream@epience.com>
2026-01-09 13:29:09 +0900

    라우터 설정 추가

--- README.md
+++ README.md
@@ -13,3 +13,12 @@ npm install
 npm run dev
 ```
 
+### Adding a Router
+``` bash
+npm install react-router-dom@6
+
+```
+  - First thing to do is create a Browser Router and configure our first route. This will enable client side routing for our web app.
+  - The main.jsx file is the entry point. Open it up and we'll put React Router on the page.
+  - This first route is what we often call the "root route" since the rest of our routes will render inside of it.
+  - It will serve as the root layout of the UI, we'll have nested layouts as we get farther along.
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,33 @@
+ 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import { 
+  createBrowserRouter,
+  RouterProvider,
+ } from 'react-router-dom'
+
 import './index.css'
-import App from './App.tsx'
+import Root from './routes/root.tsx'
+import ErrorPage from './error-page.tsx'
+import Contact from './routes/contact.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/contacts/:contactId',
+        element: <Contact />,
+      }
+    ]
+  }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>  
-    <App />  
+    <RouterProvider router={router} />  
   </StrictMode>,
 )
